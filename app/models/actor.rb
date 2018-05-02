8e5a224321ca652ca42acfefacb36d6b4e0029df
David Markel <davidgmarkel@gmail.com>
2018-05-02 21:35:03 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.map do |attribute| '#{s.name}' end
+    self.characters.map do |attribute| '#{attribute.name}' end
   end
 
 end
